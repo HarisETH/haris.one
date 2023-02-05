b27d0b5d88f769ba74878aa5ff44f2a67ff4af8f
Haris <hariskumar.eth@gmail.com>
2023-02-05 19:37:49 +0530

    Bug fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import Hero from '@/components/Hero'
-import Socials from '@/components/Socials'
-import Current from '@/components/Current'
-import Footer from '@/components/Footer'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Hero from "@/components/Hero";
+import Socials from "@/components/Socials";
+import Current from "@/components/Current";
+import Footer from "@/components/Footer";
 
-
-// const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -22,6 +21,13 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
       </Head>
