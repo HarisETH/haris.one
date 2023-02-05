fec939574bc7e1e44706f18cb6f582c4df2382e0
Haris <hariskumar.eth@gmail.com>
2023-02-05 19:46:11 +0530

    Bug fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,16 +17,16 @@ function Hero() {
           Haris <br /> Kumar N
         </h2>
         <div className="handles flex gap-2">
-          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full linkedin flex justify-center items-center ">
+          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full linkedin flex justify-center items-center dark:border-2 border-gray-300 ">
             <Icon.Linkedin className="text-gray-100" />
           </a>
-          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full instagram flex justify-center items-center ">
+          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full instagram flex justify-center items-center dark:border-2 border-gray-300 ">
             <Icon.Instagram className="text-gray-100" />
           </a>
-          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full twitter flex justify-center items-center">
+          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full twitter flex justify-center items-center dark:border-2 border-gray-300">
             <Icon.Twitter className="text-gray-100" />
           </a>
-          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full github flex justify-center items-center ">
+          <a href="https://www." className="w-9 p-2 md:w-12 md:h-12 md:p-1 h-9 bg-gray-900 rounded-full github flex justify-center items-center dark:border-2 border-gray-300 ">
             <Icon.GitHub className="text-gray-100" />
           </a>
           {/* <div className="w-8 h-8 bg-gray-900 rounded-full "></div> */}
